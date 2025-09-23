591340eb48125f9930b45325cd7a462817d50327
KamalpreetSinghVirk0022 <vkamalpreetsingh@gmail.com>
2025-09-23 11:28:26 +0530

    database connected

--- server/config/instrument.js
+++ server/config/instrument.js
@@ -1,7 +1,8 @@
 // Import with `import * as Sentry from "@sentry/node"` if you are using ESM
 import * as Sentry from "@sentry/node"
+
 Sentry.init({
-  dsn: "https://995f2663b13ad6d90011c62571ec2315@o4510057586294784.ingest.us.sentry.io/4510057592455168",
+  dsn: "https://385d6399a9c37fdfd9ea40facaed5d45@o4510057586294784.ingest.us.sentry.io/4510057632169984",
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
+import './config/instrument.js'
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDb from './config/db.js';
-
+import * as Sentry from "@sentry/node";
+import { clerkWebhooks } from './controllers/webhooks.js';
 
 // Initialize Express app
 
@@ -21,11 +23,19 @@ app.use(express.json())
 //Routes
 
 app.get('/', (req,res)=>res.send("API Working"))
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+app.post('/webhooks', clerkWebhooks)
 
 
 //Port
 
 const PORT =process.env.PORT || 5000
+
+
+Sentry.setupExpressErrorHandler(app);
+
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
