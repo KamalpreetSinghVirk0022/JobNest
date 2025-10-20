609fb7ab7b5897f97979567d2e6b6b5ff0041a0d
KamalpreetSinghVirk0022 <vkamalpreetsingh@gmail.com>
2025-10-20 22:09:07 +0530

    sentry added

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+import mongoose, { Mongoose } from "mongoose"
+
+//function to connect to mongoDB
+
+const connectDB = async () => {
+
+    mongoose.connection.on('connected', ()=>console.log('MongoDB connected'))
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/JobNest`)
+
+}
+
+export default connectDB
