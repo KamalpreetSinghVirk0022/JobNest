f68ed77861193b7cced3e0a30ffc262d268e95b1
KamalpreetSinghVirk0022 <vkamalpreetsingh@gmail.com>
2025-11-09 15:58:21 +0530

    Typo fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home.jsx";
+import Home from "./pages/Home.jsx";
 import ApplyJob from "./pages/ApplyJob.jsx";
 import Applications from "./pages/Applications.jsx";
 import RecruiterLogin from "./components/RecruiterLogin.jsx";
